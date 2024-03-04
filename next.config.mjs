52bff583f6bc5686d4ecd5f70ec4d4bd4af209cc
christyansegecs <christyansegecs@hotmail.com>
2024-03-04 19:08:51 -0300

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'localhost',
+            'digitalhippo-production.up.railway.app'
+        ]
+    }
+};
+
+export default nextConfig;
