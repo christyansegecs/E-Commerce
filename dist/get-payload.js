fbd602112b2ad0ef2f4540bc160cb428d74d4608
christyansegecs <christyansegecs@hotmail.com>
2024-03-04 22:55:05 -0300

    change in email address

--- dist/get-payload.js
+++ dist/get-payload.js
@@ -90,7 +90,7 @@ var getPayloadClient = function (_a) {
                     if (!cached.promise) {
                         cached.promise = payload_1.default.init(__assign({ email: {
                                 transport: transporter,
-                                fromAddress: 'https://e-commerce-production-ad3a.up.railway.app/',
+                                fromAddress: 'bounced@resend.dev',
                                 fromName: 'DigitalHippo'
                             }, secret: process.env.PAYLOAD_SECRET, local: (initOptions === null || initOptions === void 0 ? void 0 : initOptions.express) ? false : true }, (initOptions || {})));
                     }
