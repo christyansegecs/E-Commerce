51cd9db35aa1ba016492b29de33080a70117eb8c
Christyan Segecs <77711449+christyansegecs@users.noreply.github.com>
2024-03-07 15:00:33 -0300

    Update payment-router.ts

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -55,14 +55,6 @@ export const paymentRouter = router({
         })
       })
 
-      line_items.push({
-        price: 'price_1OCeBwA19umTXGu8s4p2G3aX',
-        quantity: 1,
-        adjustable_quantity: {
-          enabled: false,
-        },
-      })
-
       try {
         const stripeSession =
           await stripe.checkout.sessions.create({
