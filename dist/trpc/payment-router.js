0fbd363f933993d423419dac793562fa83afac12
christyansegecs <christyansegecs@hotmail.com>
2024-03-09 09:45:42 -0300

    Update payment-router.js

--- dist/trpc/payment-router.js
+++ dist/trpc/payment-router.js
@@ -90,20 +90,13 @@ exports.paymentRouter = (0, trpc_1.router)({
                                 quantity: 1,
                             });
                         });
-                        line_items.push({
-                            price: 'price_1OCeBwA19umTXGu8s4p2G3aX',
-                            quantity: 1,
-                            adjustable_quantity: {
-                                enabled: false,
-                            },
-                        });
                         _b.label = 4;
                     case 4:
                         _b.trys.push([4, 6, , 7]);
                         return [4 /*yield*/, stripe_1.stripe.checkout.sessions.create({
                                 success_url: "".concat(process.env.NEXT_PUBLIC_SERVER_URL, "/thank-you?orderId=").concat(order.id),
                                 cancel_url: "".concat(process.env.NEXT_PUBLIC_SERVER_URL, "/cart"),
-                                payment_method_types: ['card', 'paypal'],
+                                payment_method_types: ['card'],
                                 mode: 'payment',
                                 metadata: {
                                     userId: user.id,
